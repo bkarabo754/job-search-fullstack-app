16de7de01387048295aac17342a4f6f28e9af055
Bongani Masango <bkmasango26@gmail.com>
2025-02-14 17:58:02 +0200

    Authentication complete

--- app/(mainLayout)/page.tsx
+++ app/(mainLayout)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="min-h-screen w-screen flex items-center justify-center">
-      <h1>Login</h1>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
